5ce5d9cb33061f6edf2487d6e2543cf878e4f030
Vahé Krikorian <vahekrikorian@MacBook-Pro-4.local>
2024-01-24 12:12:58 +0100

    init the game page

---
+++ front/src/components/GamePage/GamePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Wrapper } from './styled';
+
+interface GamePageProps {
+
+}
+
+const GamePage: React.FC<GamePageProps> = ({
+
+}) => {
+  return (
+    <Wrapper>
+        <p>this is the game page</p>
+    </Wrapper>
+  );
+};
+
+export default GamePage;
---
+++ front/src/components/GamePage/index.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import GamePage from './GamePage';
+
+const GamePageContainer: React.FC = () => {
+
+  return (
+    <GamePage
+
+    />
+  );
+};
+
+export default GamePageContainer;
