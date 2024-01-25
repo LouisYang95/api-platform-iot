fb64fb66e430b2fd5ca8a9084e94852b148821ba
Bulbizar <adrienesteves@icloud.com>
2024-01-25 16:37:38 +0100

    pushall

--- socket/mqtt.js
+++ socket/mqtt.js
@@ -1,8 +1,11 @@
 const mqtt = require('mqtt');
-const client = mqtt.connect("mqtt://test.mosquitto.org") // OR url, options
+const client = mqtt.connect("mqtt://mqtt-dashboard.com") // OR url, options
+
 
 client.on("connect", function () {
-  console.log("connected");
+
+  console.log("Connected to HiveMQ: " + client.connected);
+
   client.subscribe('0013a20041642063', function (err) {
     if (!err) {
       client.publish('0013a20041642063', 'Hello mqtt')
@@ -12,6 +15,23 @@ client.on("connect", function () {
   client.on('message', function (topic, message) {
     // message is Buffer
     console.log(topic, message.toString())
-    client.end()
+
   })
+
+  var message = "test message";
+  var topic = "testTopic";
+
+  var options={
+    retain:true,
+    qos:1};
+    client.publish(topic, message, options);
+  var timer_id = setInterval( function() { publish(topic, message, options); } ,5000);
+
+  //publish function
+  function publish(topic, msg, options){
+      console.log("publishing", msg);
+    if (client.connected == true){
+      client.publish(topic, msg, options);
+    }
+  }
 })
